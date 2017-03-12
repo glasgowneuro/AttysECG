be99785131223de5ecb5526e2c87d7619c06bf4d
Bernd Porr <mail@berndporr.me.uk>
2017-03-12 23:34:19 +0000

    Fixed a couple of bugs in the ECGPlotFragment, for example that it plotted the ECG upside down. Pinch/Zoom works now nicely.

--- app/src/main/java/tech/glasgowneuro/attysecg/ECGPlotFragment.java
+++ app/src/main/java/tech/glasgowneuro/attysecg/ECGPlotFragment.java
@@ -1,25 +1,33 @@
 package tech.glasgowneuro.attysecg;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PanZoom;
 import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XValueMarker;
+import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.YValueMarker;
 
 /**
- * Created by bp1 on 12/03/17.
+ * Plots one PQRST complex in detail
  */
 
 public class ECGPlotFragment extends Fragment {
@@ -32,9 +40,13 @@ public class ECGPlotFragment extends Fragment {
 
     private XYPlot pqrstPlot = null;
 
+    private PanZoom panZoom = null;
+
     private Spinner maxYspinner = null;
 
     private Spinner leadSpinner = null;
+
+    private Button resetButton = null;
 
     private ToggleButton recordButton = null;
 
@@ -42,7 +54,7 @@ public class ECGPlotFragment extends Fragment {
 
     private float[] leadsBuffer = new float[6];
 
-    private static String[] MAXY = {"0.2 mV", "0.5 mV", "1.0 mV", "1.2 mV", "1.5 mV", "2.0 mV"};
+    private static String[] MAXY = {"0.2 mV", "0.45 mV", "0.9 mV", "1.35 mV", "1.8 mV", "2.25 mV"};
 
     View view = null;
 
@@ -51,8 +63,8 @@ public class ECGPlotFragment extends Fragment {
 
     void setScale() {
         if (pqrstPlot != null) {
-            pqrstPlot.setRangeBoundaries(range, -range, BoundaryMode.FIXED);
-            pqrstPlot.setDomainBoundaries(-displayTime/2,displayTime/2, BoundaryMode.FIXED);
+            pqrstPlot.setRangeBoundaries(-range, range, BoundaryMode.FIXED);
+            pqrstPlot.setDomainBoundaries(-displayTime / 2, displayTime / 2, BoundaryMode.FIXED);
             pqrstPlot.redraw();
         }
     }
@@ -63,12 +75,12 @@ public class ECGPlotFragment extends Fragment {
     private int inPtr = 0;
     private int rPtr = -1;
     private int rCtr = 0;
-    private float displayTime = 2;
+    private float displayTime = 0.45F*2;
     private int nSamplesDispl = 1000;
 
     public void setSamplingRate(float _samplingrate) {
         samplingRate = _samplingrate;
-        nSamplesDispl = (int)(samplingRate*displayTime);
+        nSamplesDispl = (int) (samplingRate * displayTime);
         ringBufferSize = (int) (samplingRate * 10);
         ringBuffer = new float[ringBufferSize];
     }
@@ -104,6 +116,9 @@ public class ECGPlotFragment extends Fragment {
             }
         }
 
+        panZoom = PanZoom.attach(pqrstPlot, PanZoom.Pan.BOTH, PanZoom.Zoom.STRETCH_BOTH);
+        panZoom.setEnabled(true);
+
         pqrstSeries = new SimpleXYSeries("");
         if (pqrstSeries == null) {
             if (Log.isLoggable(TAG, Log.ERROR)) {
@@ -116,15 +131,67 @@ public class ECGPlotFragment extends Fragment {
                 new LineAndPointFormatter(
                         Color.rgb(100, 255, 255), null, null, null));
         pqrstPlot.setDomainLabel("t/sec");
-        pqrstPlot.setRangeLabel(LEAD[lead]+"/mV");
+        pqrstPlot.setRangeLabel(LEAD[lead] + "/mV");
 
         Paint paint = new Paint();
         paint.setColor(Color.argb(128, 0, 255, 0));
         pqrstPlot.getGraph().setDomainGridLinePaint(paint);
         pqrstPlot.getGraph().setRangeGridLinePaint(paint);
 
+        XYGraphWidget.LineLabelRenderer lineLabelRendererY = new XYGraphWidget.LineLabelRenderer() {
+            @Override
+            public void drawLabel(Canvas canvas,
+                                  XYGraphWidget.LineLabelStyle style,
+                                  Number val, float x, float y, boolean isOrigin) {
+                Rect bounds = new Rect();
+                style.getPaint().getTextBounds("a", 0, 1, bounds);
+                drawLabel(canvas, String.format("%02.4f ", val.floatValue()),
+                        style.getPaint(), x + bounds.width() / 2, y + bounds.height(), isOrigin);
+            }
+        };
+        pqrstPlot.getGraph().setLineLabelRenderer(XYGraphWidget.Edge.LEFT, lineLabelRendererY);
+
+        XYGraphWidget.LineLabelStyle lineLabelStyleY = pqrstPlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT);
+        Rect bounds = new Rect();
+        String dummyTxt = String.format("%02.4f ", 0.000597558899);
+        lineLabelStyleY.getPaint().getTextBounds(dummyTxt, 0, dummyTxt.length(), bounds);
+        pqrstPlot.getGraph().setMarginLeft(bounds.width());
+
+
+        XYGraphWidget.LineLabelRenderer lineLabelRendererX = new XYGraphWidget.LineLabelRenderer() {
+            @Override
+            public void drawLabel(Canvas canvas,
+                                  XYGraphWidget.LineLabelStyle style,
+                                  Number val, float x, float y, boolean isOrigin) {
+                Rect bounds = new Rect();
+                style.getPaint().getTextBounds("a", 0, 1, bounds);
+                if (!isOrigin) {
+                    final int canvasState = canvas.save();
+                    canvas.rotate(style.getRotation(), x, y);
+                    drawLabel(canvas, String.format("%02.4f ", val.floatValue()),
+                            style.getPaint(), x + bounds.width() / 2, y + bounds.height(), isOrigin);
+                    canvas.restoreToCount(canvasState);
+                }
+            }
+        };
+        pqrstPlot.getGraph().setLineLabelRenderer(XYGraphWidget.Edge.BOTTOM, lineLabelRendererX);
+
+        XYGraphWidget.LineLabelStyle lineLabelStyleX = pqrstPlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.LEFT);
+        bounds = new Rect();
+        dummyTxt = String.format("%02.4f ", 0.000597558899);
+        lineLabelStyleX.getPaint().getTextBounds(dummyTxt, 0, dummyTxt.length(), bounds);
+        pqrstPlot.getGraph().setMarginBottom(bounds.width());
+
+
         recordButton = (ToggleButton) view.findViewById(R.id.ecgplotfragment_record);
         recordButton.setChecked(true);
+
+        resetButton = (Button) view.findViewById(R.id.ecgplotfragment_reset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setScale();
+            }
+        });
 
         leadSpinner = (Spinner) view.findViewById(R.id.ecgplotfragment_lead);
         ArrayAdapter<String> adapterChannel = new ArrayAdapter<>(getContext(),
@@ -170,13 +237,11 @@ public class ECGPlotFragment extends Fragment {
         maxYspinner.setBackgroundResource(android.R.drawable.btn_default);
         maxYspinner.setSelection(1);
 
-
         rPtr = -1;
 
         return view;
 
     }
-
 
     public synchronized void redraw() {
 
@@ -185,7 +250,7 @@ public class ECGPlotFragment extends Fragment {
         }
     }
 
-    public synchronized void addValue(float I,float II,float III,float aVR,float aVL,float aVF) {
+    public synchronized void addValue(float I, float II, float III, float aVR, float aVL, float aVF) {
         leadsBuffer[0] = I;
         leadsBuffer[1] = II;
         leadsBuffer[2] = III;
@@ -200,26 +265,23 @@ public class ECGPlotFragment extends Fragment {
         }
         if (rPtr > -1) {
             rCtr--;
-            //Log.d(TAG,"rCtr="+rCtr);
             if (rCtr == 0) {
                 if (pqrstSeries == null) {
                     return;
                 }
-                // get rid the oldest sample in history:
                 while (pqrstSeries.size() > 0) {
                     pqrstSeries.removeFirst();
                 }
 
                 int outPtr = rPtr;
-                for (int i=0;i<(nSamplesDispl/2);i++) {
+                for (int i = 0; i < (nSamplesDispl / 2); i++) {
                     outPtr--;
                     if (outPtr < 0) {
-                        outPtr = ringBufferSize-1;
+                        outPtr = ringBufferSize - 1;
                     }
                 }
-                for (int i = 0; i < nSamplesDispl; i++) {
-                    // add the latest history sample:
-                    pqrstSeries.addLast((i-nSamplesDispl/2)/samplingRate, ringBuffer[outPtr]*1000);
+                for (int i = 0; i < (nSamplesDispl-1); i++) {
+                    pqrstSeries.addLast((i - nSamplesDispl / 2) / samplingRate, ringBuffer[outPtr] * 1000);
                     outPtr++;
                     if (outPtr == ringBufferSize) {
                         outPtr = 0;
@@ -240,6 +302,5 @@ public class ECGPlotFragment extends Fragment {
             rCtr = nSamplesDispl / 2;
         }
     }
-
 
 }
