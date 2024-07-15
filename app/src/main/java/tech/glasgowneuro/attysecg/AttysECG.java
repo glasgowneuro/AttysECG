9c7c3fb35ee7ca0ef10f117a53085061fa892284
Bernd Porr <mail@berndporr.me.uk>
2024-07-15 21:35:36 +0100

    Removed one unused import

--- app/src/main/java/tech/glasgowneuro/attysecg/AttysECG.java
+++ app/src/main/java/tech/glasgowneuro/attysecg/AttysECG.java
@@ -40,7 +40,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
